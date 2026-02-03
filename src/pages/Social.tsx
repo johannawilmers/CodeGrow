24a617e9693923d84827f150bf3e4a03bc10be50
johanwil <johanwil@stud.ntnu.no>
2026-02-03 12:39:56 +0100

    Set back social page

--- src/pages/Social.tsx
+++ src/pages/Social.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import FriendsSuggestions from "../components/FriendsSuggestions";
+
 
 const Social: React.FC = () => {
   return (
     <div className="main-content">
       <h1>Social</h1>
-      <FriendsSuggestions />
+      <p>Coming soon!</p>
     </div>
   );
 };
