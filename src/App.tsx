321d1fe71d3f457bf644b1f0cf0d9c4b46b12fa8
johanwil <johanwil@stud.ntnu.no>
2026-02-24 14:03:33 +0100

    build errors

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ import Login from "./components/Login";
 import TasksByTopicPage from "./pages/TaskByTopic";
 
 import { validateStreak } from "./utils/validateStreak";
-import ThemeTopicManager from "./pages/ThemeTopicManager";
-import AdminPage from "./pages/AdminPage";
 
 function App() {
   useEffect(() => {
@@ -34,13 +32,12 @@ function App() {
           <Route path="/task/:taskId" element={<TaskPage />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/social" element={<Social />} />
-          <Route path="/admin" element={<AdminPage />} />
           
           <Route
             path="/topic/:topicId/tasks"
             element={<TasksByTopicPage />}
           />
-          <Route path="/admin/themes-topics" element={<ThemeTopicManager />} />
+
 
         </Routes>
       </Login>
