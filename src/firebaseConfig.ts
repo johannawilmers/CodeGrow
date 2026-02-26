0bc8a3563ea329342feec88ea7c6156e8fe0aa56
johanwil <johanwil@stud.ntnu.no>
2026-02-26 12:42:34 +0100

    config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,12 @@
+// Firebase configuration secrets
+// This file is in .gitignore to keep sensitive data out of version control
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBlIltIs43cIXDG0Xyhn1bHPQcNVJsgefA",
+  authDomain: "codegrow-5894a.firebaseapp.com",
+  projectId: "codegrow-5894a",
+  storageBucket: "codegrow-5894a.firebasestorage.app",
+  messagingSenderId: "717569559294",
+  appId: "1:717569559294:web:724fb892509337a383b3af",
+  measurementId: "G-5BJVRH8BBG"
+};
